4973182ee6cfeac05d983c1d4ab11c77f3385fcb
ablo92 <bigsow92@gmail.com>
2015-06-27 01:28:16 +0300

    Added ShoppingListItem Data Model

---
+++ app/src/main/java/com/shoplist/hackcyprus/shoplistapp/data/model/ShoppingListItem.java
@@ -0,0 +1,43 @@
+package com.shoplist.hackcyprus.shoplistapp.data.model;
+
+/**
+ * Created by Abdoulaye Sow on 6/27/2015.
+ */
+public class ShoppingListItem {
+    private int id;
+    private string name;
+    private int quantity;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
